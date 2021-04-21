d79669760e041eb144d0cc65e68a0c8b8b1b495e
Tushar1401 <tushararora1401@gmail.com>
2021-04-21 13:26:01 +0530

    Updates

---
+++ precache-manifest.da11241cedb437f70a89b37ac14e6650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eee84ed1ad38ebaf78553f3b43e6ae0",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "f84546aa2f7a85f39a48",
+    "url": "/airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "99d3827f14ef94f01d6b",
+    "url": "/airbnb-clone/static/css/main.5770747e.chunk.css"
+  },
+  {
+    "revision": "f84546aa2f7a85f39a48",
+    "url": "/airbnb-clone/static/js/2.b044fd61.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.b044fd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d3827f14ef94f01d6b",
+    "url": "/airbnb-clone/static/js/main.699db59a.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
